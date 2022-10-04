e3175958c41260f075ddf6658f179ef0b77562a4
wendelsilva <wendel245.ws@gmail.com>
2022-10-04 14:51:40 -0300

    create register user route and setup password storage with bcrypt

--- server/src/User/Controller.ts
+++ server/src/User/Controller.ts
@@ -1,13 +1,14 @@
-import { routes } from "../routes";
-import { prisma } from "../prisma";
-import jwt from "jsonwebtoken"
+import { routes } from '../routes';
+import { prisma } from '../prisma';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
-const secret = String(process.env.VITE_SECRET_KEY)
+const secret = String(process.env.VITE_SECRET_KEY);
 
 routes.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
-    if(email != undefined && password != undefined) {
+    if(email && password) {
         const user = await prisma.user.findUnique({
             where: {
                 email,
@@ -15,7 +16,9 @@ routes.post('/login', async (req, res) => {
         });
 
         if(user) {
-            if(user.password === password) {
+            const passwordIsCorrect = bcrypt.compareSync(password, user.password);
+            
+            if(passwordIsCorrect) {
                 jwt.sign({id: user.id, email: user.email}, secret, {expiresIn: '48h'}, (error, token) => {
                     if(error) {
                         res.status(400).send({error: 'internal error'});
@@ -26,12 +29,45 @@ routes.post('/login', async (req, res) => {
             } else {
                 res.status(401).send({error: 'invalid credentials'});
             }
+
         } else {
             res.status(404).send({error: 'user not found'});
         }
     } else {
         res.status(400).send({error: 'internal error'});
     }
+});
+
+routes.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if( name && email && password ) {
+        const hasDuplicatedEmail = await prisma.user.findUnique({
+            where: { 
+                email,
+            }
+        });
+
+        if(hasDuplicatedEmail) {
+            res.status(409).send({error: 'duplicated email'});
+        } else {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+
+            await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: hash,
+                }
+            });
+
+            res.status(201).send();
+        }
+    } else {
+        res.status(400).send({error: 'internal error'});
+    }
+
 });
 
 module.exports = routes;
